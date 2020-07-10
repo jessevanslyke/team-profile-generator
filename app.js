cfde7b95ec4ceed85f1f1b31fec17a5ed4caaaef
Jesse VanSlyke <jesse.j.vanslyke@gmail.com>
2020-07-10 00:25:51 -0400

    Finished app.js to generate the html based on the input from inquierer

--- app.js
+++ app.js
@@ -9,27 +9,98 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
 
+// Push any new Employees into this array
+var employeeArr = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+async function addEmployee() {
+    // Setup the newEmployee variable
+    var newEmployee;
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    // Start our initial inquierer prompt questions for all employees
+    await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the employee's name: "
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the employee's id : "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the employee's email: "
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "Enter the employee's role: ",
+            choices: ["Manager", "Engineer", "Intern"],
+        }
+    ])
+    .then(function(answers) {
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+        // Depending on the answers.role, we ask 1 more question
+        inquirer.prompt([
+            {
+                type: "input",
+                name: answers.role === "Manager" ? "officeNumber" : 
+                    answers.role === "Engineer" ? "github" : 
+                    "school",
+                message: answers.role === "Manager" ? "Enter the manager's office number: " :
+                        answers.role === "Engineer" ? "Enter the engineer's github: " : 
+                        "Enter the intern's school: "
+            }])
+        .then(function(moreAnswers) {
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+            // Create a new instance of an object, based on the employee role
+            if (answers.role === "Manager"){
+                newEmployee = new Manager(answers.name, answers.id, answers.email, moreAnswers.officeNumber);
+                console.log("New manager: ", newEmployee);
+            }
+            if (answers.role === "Engineer"){
+                newEmployee = new Engineer(answers.name, answers.id, answers.email, moreAnswers.github);
+                console.log("New engineer: ", newEmployee);
+            }
+            if (answers.role === "Intern"){
+                newEmployee = new Intern(answers.name, answers.id, answers.email, moreAnswers.school);
+                console.log("New intern: ", newEmployee);
+            }
+            employeeArr.push(newEmployee);
+            checkForDone();
+        }) 
+    })
+}
+
+function checkForDone() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "done",
+        message: "Do you need to add another employee to the team?",
+        default: true
+    }])
+    .then(function(answers) {
+        if (answers.done){
+            addEmployee();
+            return;
+        }
+        else {
+            html = render(employeeArr);
+            fs.writeFile(outputPath, html, function(err) {
+                if (err)
+                    console.log(err);
+                    
+                console.log("team.html saved to: ", outputPath);
+            })
+        }
+    })
+}
+
+addEmployee();
